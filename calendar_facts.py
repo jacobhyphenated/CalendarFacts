813670df6a9e562d05c937cc19cc102505b2f8c4
Jacob Kanipe-Illig <jacobhyphenated@gmail.com>
2017-12-18 14:34:11 -0500

    completed the tree of options.

--- calendar_facts.py
+++ calendar_facts.py
@@ -82,8 +82,134 @@ allOptions = {
     16: {
         'values': [('moon', 20)]
     },
+
     20: {
-        'values':[('')],
+        'values':[
+            ('happens', 21),
+            ('drifts out of sync with the', 22),
+            ('might', 23)
+        ]
+    },
+    21: {
+        'values':[
+            ('earlier', 24),
+            ('later', 24),
+            ('at the wrong time', 24)
+        ]
+    },
+    22: {
+        'values': [
+            ('sun', 40),
+            ('moon', 40),
+            ('Zodiac', 40),
+            ('Gregorian', 25),
+            ('Mayan', 25),
+            ('Lunar', 25),
+            ('iPhone', 25),
+            ('atomic clock in Colorado', 40),
+        ]
+    },
+    23: {
+        'values': [
+            ('not happen', 26),
+            ('happen twice', 26),
+        ]
+    },
+    24: {
+        'values': [('every year', 40)]
+    },
+    25: {
+        'values': [('calendar', 40)]
+    },
+    26: {
+        'values': [('this year', 40)]
+    },
+
+    40: {
+        'values': [('because of', 41)]
+    },
+    41: {
+        'values': [
+            ('time zone legislation in', 42),
+            ('a decree by the pope in the 1500s', 60),
+            ('precession', 43),
+            ('libration', 43),
+            ('nutation', 43),
+            ('libation', 43),
+            ('ecentricity', 43),
+            ('obliquity', 43),
+            ('magnetic field reversal', 60),
+            ('an arbitrary decision by', 44)
+
+        ]
+    },
+    42: {
+        'values': [
+            ('Indiana', 60),
+            ('Arizona', 60),
+            ('Russia', 60)
+        ]
+    },
+    43: {
+        'values': [('of the', 45)]
+    },
+    44: {
+        'values': [
+            ('Benjamin Franklin', 60),
+            ('Isaac Newton', 60),
+            ('FDR', 60)
+        ]
+    },
+    45: {
+        'values': [
+            ('Moon', 60),
+            ('Sun', 60),
+            ('Earth\'s axis', 60),
+            ('equator', 60),
+            ('prime meridian', 60),
+            ('international date', 46),
+            ('Mason-Dixon', 46)
+        ]
+    },
+    46: {
+        'values': [('line', 60)]
+    },
+
+    60: {
+        'values': [('', 61)],
+        'punctuation': '?'
+    },
+    61: {
+        'values': [('Apparently', 62)]
+    },
+    62: {
+        'values': [
+            ('it causes a predicatble increase in car accidents', 80),
+            ('that\'s why we have leap seconds', 80),
+            ('scientists are really worried', 80),
+            ('it was even more extreme during the', 63),
+            ('there\'s a proposal to fix it, but it', 64),
+            ('it\'s getting worse and no one knows why', 80)
+        ]
+    },
+    63: {
+        'values': [
+            ('Bronze Age', 80),
+            ('Ice Age', 80),
+            ('Cretaceous', 80),
+            ('1990s', 80)
+        ]
+    },
+    64: {
+        'values': [
+            ('will never happen', 80),
+            ('actually makes things worse', 80),
+            ('is stalled in congress', 80),
+            ('might be unconstitutional', 80)
+        ]
+    },
+
+    80: {
         'end': True,
         'punctuation': '.'
     }
